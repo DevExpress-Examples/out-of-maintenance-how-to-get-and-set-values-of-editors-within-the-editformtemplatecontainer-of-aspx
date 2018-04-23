f8a2527dfe524c9e1cd76d924bdafd348cf1b194
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebApplication2/Default.aspx.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxEditors;
+
+namespace WebApplication2
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e) {
+        }
+
+        protected void ASPxGridView1_AfterPerformCallback(object sender, DevExpress.Web.ASPxGridView.ASPxGridViewAfterPerformCallbackEventArgs e) {
+            if (e.CallbackName == "STARTEDIT") {
+                int index = ((ASPxGridView)sender).EditingRowVisibleIndex;
+                string val = ((ASPxGridView)sender).GetRowValues(index, "Description").ToString();
+                ASPxTextBox box = ((ASPxGridView)sender).FindEditFormTemplateControl("ASPxTextBox2") as ASPxTextBox;
+                box.Text = val;
+            }
+        }
+        protected void ASPxGridView1_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
+            ASPxTextBox box = ((ASPxGridView)sender).FindEditFormTemplateControl("ASPxTextBox2") as ASPxTextBox;
+            e.NewValues["Description"] = box.Text;
+        }
+    }
+}
---
+++ VB/WebApplication2/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+
+Namespace WebApplication2
+    Partial Public Class _Default
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        End Sub
+
+        Protected Sub ASPxGridView1_AfterPerformCallback(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxGridView.ASPxGridViewAfterPerformCallbackEventArgs)
+            If e.CallbackName = "STARTEDIT" Then
+                Dim index As Integer = DirectCast(sender, ASPxGridView).EditingRowVisibleIndex
+                Dim val As String = DirectCast(sender, ASPxGridView).GetRowValues(index, "Description").ToString()
+                Dim box As ASPxTextBox = TryCast(DirectCast(sender, ASPxGridView).FindEditFormTemplateControl("ASPxTextBox2"), ASPxTextBox)
+                box.Text = val
+            End If
+        End Sub
+        Protected Sub ASPxGridView1_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+            Dim box As ASPxTextBox = TryCast(DirectCast(sender, ASPxGridView).FindEditFormTemplateControl("ASPxTextBox2"), ASPxTextBox)
+            e.NewValues("Description") = box.Text
+        End Sub
+    End Class
+End Namespace
